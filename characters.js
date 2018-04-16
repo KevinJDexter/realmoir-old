4c31de0f66359683953b4063cc2ab4d1e5c3f47e
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-16 14:26:13 -0500

    Efforts to create a persistent Character array

---
+++ characters.js
@@ -0,0 +1,11 @@
+const characterList = [];
+
+class Character {
+  constructor (name, birth, death, age, home) {
+    this.name = name;
+    this.birth = birth;
+    this.death = death;
+    this.age = age;
+    this.home = home;
+  }
+}
---
+++ charpage.js
@@ -0,0 +1,16 @@
+$(document).ready(onReady);
+
+function onReady () {
+  for (let character of characterList) {
+    let newListElement = '<ul>';
+    newListElement += '<li>Name: ' + character.name + '</li>';
+    newListElement += '<li>Born: ' + character.born + '</li>';
+    newListElement += '<li>Died: ' + character.death + '</li>';
+    newListElement += '<li>Age: ' + character.age + '</li>';
+    newListElement += '<li>Home: ' + character.home + '</li>';
+    newListElement += '</ul>';
+
+    $('body').append(newListElement);
+  }
+  console.log(characterList);
+}
